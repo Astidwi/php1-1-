cccc57cdd1efae83a31b640b5e40ceb4e3c49093
Astidwi <53321153+Astidwi@users.noreply.github.com>
2019-10-04 09:32:01 +0700

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,20 @@
+<?php
+    //echo "Hello world-!";
+    //echo "<h1>Hello world<h1>-!";
+
+    //VARIABEL
+    //diawali dengan $
+    //nama variabel tidak boleh dawali dengan angka
+    //tidak boleh mengandung angka
+    //$nama = "Asti dwi febrianti";
+    //$pesan = "Hallo selamat datang <strong>$nama</strong>";
+
+    //echo $pesan;
+
+    //OPRATOR ARITMETIKA
+    // + - * / %{modulus}
+    $a = 10;
+    $b = 20;
+    echo $a % $b;
+
+?>
